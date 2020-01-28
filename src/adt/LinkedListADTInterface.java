050afac10c8ff4bbd02c9937db489717d457141b
dbc2201 <divyanshb22@gmail.com>
2020-01-29 00:19:52 +0530

    add JavaDoc comment for the ADT

--- src/adt/LinkedListADTInterface.java
+++ src/adt/LinkedListADTInterface.java
@@ -1,4 +1,14 @@
 package adt;
 
+/**
+ * This interface will serve as the ADT for the LinkedList.
+ * This ADT will define the operations available on the LinkedList.
+ * As all the other Data Structures, the LinkedList will have operations to:
+ * 1. add a node in the linked list.
+ * 2. delete a node from the linked list.
+ * 3. search a node in the linked list.
+ * 4. sort the whole linked list based on the type of data its nodes contain.
+ * 5. traverse the whole linked list and print the contents of its nodes.
+ */
 public interface LinkedListADTInterface<E> {
 }
