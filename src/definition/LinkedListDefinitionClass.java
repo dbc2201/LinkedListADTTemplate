7816760bfe47e1a10c8fca9442f509d36175665c
dbc2201 <divyanshb22@gmail.com>
2020-01-29 00:48:15 +0530

    create a class to define the linked list

---
+++ src/definition/LinkedListDefinitionClass.java
@@ -0,0 +1,11 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: dbc2201
+ *   Date: 29/01/20
+ *   Time: 12:48 AM
+ */
+
+package definition;
+
+public class LinkedListDefinitionClass {
+}
